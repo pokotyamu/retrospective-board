43addd41af8ce411da10d448dd197165fe22b0e1
pokotyamu <pokotyamu@gmail.com>
2020-02-10 00:05:12 +0900

    feat: create comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,22 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:edit, :update]
+  before_action :set_card, only: [:new, :create]
+
+  def new
+    @comment = @card.comments.build
+  end
+
+  def create
+    comment = @card.comments.build(comment_params)
+
+    respond_to do |format|
+      if comment.save
+        format.html { redirect_to @card.board, notice: 'Comment was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 
   def edit
   end
@@ -17,7 +34,11 @@ class CommentsController < ApplicationController
   private
 
   def set_comment
-    @comment = Comment.find(params['id'])
+    @comment = Comment.find(params[:id])
+  end
+
+  def set_card
+    @card = Card.find(params[:card_id])
   end
 
   def comment_params
